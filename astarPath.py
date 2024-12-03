263dfa43da73fff73f77be018214c558f3f264db
Shishimi <103904108+Shishimi@users.noreply.github.com>
2024-12-02 21:11:35 -0500

    Add files via upload

--- astarPath.py
+++ astarPath.py
@@ -1,4 +1,5 @@
 import heapq
+import math
 from cmath import sqrt
 
 import osmnx as ox
@@ -12,8 +13,22 @@ G = ox.graph_from_point(point_A, dist=4000, network_type='drive', retain_all=Tru
 node_A = ox.distance.nearest_nodes(G, point_A[1], point_A[0])  # lon, lat
 node_B = ox.distance.nearest_nodes(G, point_B[1], point_B[0])  # lon, lat
 
+def heuristic (current, target):
+    node_data_current = G.nodes[current]
+    nearest_lat_current = node_data_current['y']
+    nearest_lon_current = node_data_current['x']
 
-def astar_path_osmnx(G, source, target, heuristic=None, weight='length'):
+    node_data_target = G.nodes[target]
+    nearest_lat_target = node_data_target['y']
+    nearest_lon_target = node_data_target['x']
+
+    dx = nearest_lon_target - nearest_lon_current
+    dy = nearest_lat_target - nearest_lat_current
+    distance = math.sqrt(dx ** 2 + dy ** 2)
+
+    return distance
+
+def astar_path_osmnx(G, source, target, heuristic, weight='length'):
 
     visited = []
     heapq.heappush(visited, (0, source))
@@ -36,7 +51,6 @@ def astar_path_osmnx(G, source, target, heuristic=None, weight='length'):
         for neighbor in G.neighbors(current):
             edge_weight = G[current][neighbor].get(weight, 1)
             tentative_g_score = g_score[current] + edge_weight
-
             estimated_cost = heuristic(neighbor, target)
             f_score = tentative_g_score + estimated_cost
 
@@ -47,4 +61,4 @@ def astar_path_osmnx(G, source, target, heuristic=None, weight='length'):
 
     raise nx.NetworkXNoPath(f"No path between {source} and {target}.")
 
-# astar_path_osmnx(G, node_A, node_B)
+print(astar_path_osmnx(G, node_A, node_B, heuristic, weight='length'))
